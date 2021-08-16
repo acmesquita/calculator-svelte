61e46d94fea100a584f06520f05207692bb71b47
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-08-16 11:28:43 -0300

    craindo uma calculadora com svelte

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Calculate from "./components/calculate.svelte";
+</script>
+
+<main>
+	<Calculate />
+</main>
+
+<style>
+	:global(body) {
+		background-color: #111;
+	}
+
+	:root {
+		--backgroundColor: #555;
+	}
+
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+</style>
